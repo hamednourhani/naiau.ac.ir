03174ec2829c4f1dbf1f8a363144f873874a5c6f
Hamed Nourhani <info@itstar.ir>
2015-08-08 18:29:26 +0430

    3

--- index.php
+++ index.php
@@ -1,76 +1,108 @@
-<?php get_header(); ?>
+<!doctype html>
 
-			<div id="content">
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-				<div id="inner-content" class="wrap cf">
+	<head>
+		<meta charset="utf-8">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<title><?php wp_title(''); ?></title>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-								<header class="article-header">
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
 
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-                                        				<?php printf( __( 'Posted %1$s by %2$s', 'naiau' ),
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-								</header>
+		<?php // wordpress head functions ?>
+		<?php wp_head(); ?>
+		<?php // end of wordpress head ?>
 
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
 
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'naiau' ), __( '<span>One</span> Comment', 'naiau' ), __( '<span>%</span> Comments', 'naiau' ) );?>
-									</p>
+	</head>
 
+	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'naiau' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+		<div class="body-wrapper">
+		<!-- <img src="images/introBack.jpg" class="back-wall"/> -->
+			
+			<header class="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'naiau' ) . '</span> ', ', ', '</p>' ); ?>
+				<div id="top-bar" class="top-bar cf">
+				<section class="layout">
+					
+					<nav role="navigation" class = "top-bar-menu" itemscope itemtype="http://schema.org/SiteNavigationElement">
+						
+						<?php wp_nav_menu(array(
+    					         'container' => false,                           // remove nav container
+    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					         'menu' => __( 'The Main Menu', 'naiau' ),  // nav name
+    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+    					         'theme_location' => 'top-nav',                 // where it's located in the theme
+    					         'before' => '',                                 // before the menu
+        			               'after' => '',                                  // after the menu
+        			               'link_before' => '',                            // before each link
+        			               'link_after' => '',                             // after each link
+        			               'depth' => 3,                                   // limit the depth of the nav
+    					         'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
 
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php naiau_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'naiau' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'naiau' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'naiau' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-
-						</main>
-
-					<?php get_sidebar(); ?>
-
+					</nav>
+					<div class="search-area">
+						<img  class="search-icon" src="<?php echo get_template_directory_uri();?>/images/search-icon.png" />
+						<input type="search" class="top-search" placeholder="search" />
+						
+					</div>
+				</section>
 				</div>
 
-			</div>
+				<div id="menu-bar" class="menu-bar cf">
+					<section class="layout">
+						<div class="logo-wrapper">
+							<img src="<?php echo get_template_directory_uri();?>/images/nazarabad-logo.png" />
+						</div>
+						<nav role="navigation" class = "main-menu" itemscope itemtype="http://schema.org/SiteNavigationElement">
+							
+							<?php wp_nav_menu(array(
+	    					         'container' => false,                           // remove nav container
+	    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+	    					         'menu' => __( 'The Main Menu', 'naiau' ),  // nav name
+	    					         'menu_class' => 'nav main-nav cf',               // adding custom nav class
+	    					         'theme_location' => 'main-nav',                 // where it's located in the theme
+	    					         'before' => '',                                 // before the menu
+	        			               'after' => '',                                  // after the menu
+	        			               'link_before' => '',                            // before each link
+	        			               'link_after' => '',                             // after each link
+	        			               'depth' => 3,                                   // limit the depth of the nav
+	    					         'fallback_cb' => ''                             // fallback function (if there is one)
+							)); ?>
 
+						</nav>
+					</section>
+				</div>
 
-<?php get_footer(); ?>
+			</header>
+		
+
+		</div>
+	</body>
+</html>
